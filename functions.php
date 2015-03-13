1c533be0c7092f98e43531b644b8c641f06baee6
jsrothwell <jamiesonrothwell@gmail.com>
2015-03-12 22:58:09 -0700

    Fixed dropdown bug. Works

--- functions.php
+++ functions.php
@@ -100,10 +100,14 @@ add_action( 'widgets_init', 'jellicle_widgets_init' );
  * Enqueue scripts and styles.
  */
 function jellicle_scripts() {
+    wp_enqueue_style( 'bootstrap', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css' );
 	wp_enqueue_style( 'jellicle-style', get_stylesheet_uri() );
+    wp_enqueue_script( 'jquery', '//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', array('jquery'), '1.9.1', true); // we need the jquery library for bootsrap js to function
+wp_enqueue_script( 'bootstrap-js', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js', array('jquery'), true); // all the bootstrap javascript goodness
 	wp_enqueue_script( 'jellicle-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( 'jellicle-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+    wp_enqueue_script( 'jellicle-production', get_template_directory_uri() . '/js/production.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
